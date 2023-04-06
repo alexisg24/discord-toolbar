1bcb266edfef28785abc9a81d7c5ecb6c8880197
Alexis Garcia <azoreno2@gmail.com>
2023-04-06 13:05:28 -0400

    :bug: Buttons bug
    Fixed discord button order array

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 chrome.runtime.onMessage.addListener((req, res, next) => {
     const buttons = document.querySelectorAll('.button-12Fmur');
     if(req.type === 'mic'){
-        const muteButton = buttons[0];
+        const muteButton = buttons[1];
         muteButton.click();
         const status = muteButton.getAttribute('aria-checked')
         next(status)
     }else if(req.type === 'ask'){
-        next([buttons[0].getAttribute('aria-checked'), buttons[1].getAttribute('aria-checked')])
+        next([buttons[1].getAttribute('aria-checked'), buttons[2].getAttribute('aria-checked')])
     }else{
-        const deafenButtom = buttons[1];
-        const muteButton = buttons[0];
+        const deafenButtom = buttons[2];
+        const muteButton = buttons[1];
         deafenButtom.click();
         const status = deafenButtom.getAttribute('aria-checked')
         const status2 = muteButton.getAttribute('aria-checked')
